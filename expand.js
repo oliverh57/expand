990b6e1712352d75fddf18da8976305a4b38751d
Oliver Hynds <oliver.hynds@gmail.com>
2017-10-30 20:38:10 +0000

    Update expand.js
    updates webpage with updates on code

--- expand.js
+++ expand.js
@@ -1,34 +1,29 @@
 var x_power;
 var y_power;
+var finished = true;
 
 function value(row, index) {
-	if (index < 0 || index > row) {
-		return 0;
-	}
-	if (index === 1 || index === row) {
-		return 1;
-	}
-	var result = value(row - 1, index - 1) + value(row - 1, index);
-	return result;
+    if (index < 0 || index > row) {
+        return 0;
+    }
+    if (index === 1 || index === row) {
+        return 1;
+    }
+    var result = value(row - 1, index - 1) + value(row - 1, index);
+    return result;
 }
 
 
 function pasc(n) {
-	var r = [];
-	for (var i = 1; i <= n; i++) {
-		x_power = n - i;
-		y_power = i - 1;
-		r.push(value(n, i) + "x^" + x_power + "*y^" + y_power);
-	}
-	return r;
-}
+    finished = false;
+    var r = [];
+    for (var i = 1; i <= n; i++) {
+        x_power = n - i;
+        y_power = i - 1;
+        r.push(value(n, i) + "x^" + x_power + "*y^" + y_power);
+    }
+    document.getElementById("submit").classList.remove('is-loading');
+    return r;
+    finished = true;
 
-
-function dissplay(n) {
-	document.write(pasc(n));
-	console.log(pasc(n));
-}
-
-function solve(s) {
-	dissplay(s + 1)
 }
